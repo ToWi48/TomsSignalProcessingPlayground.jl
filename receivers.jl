fb0636970ecc29feeac86221e9ac5de3d2c99811
Tom Wiessner <tom.wiessner@outlook.de>
2025-07-01 00:15:26 +0200

    added first IQ demod approach

--- receivers.jl
+++ receivers.jl
@@ -46,6 +46,8 @@ f_rf_img = f_lo-f_if+1
 s_rf_img = cos.(2*pi*f_rf_img*t.+deg2rad(p_rf_img))
 s_rf_img_spec = fftshift(1 / length(s_rf_img) * fft(s_rf_img))
 
+s_in = s_rf# + s_rf_img
+
 # plot results
 plot_time = plot(
     [
@@ -54,7 +56,7 @@ plot_time = plot(
         scatter(x=Fs/N*(-N/2:N/2-1), y=abs.(s_rf_img_spec), name="RF* Signal")
     ], 
     Layout(
-        title="Zeitsignal",
+        title="Source Signals",
         xaxis_title="Frequency (Hz)",
         yaxis_title="Amplitude",
         yaxis=attr(range=[0, 1])
@@ -64,7 +66,6 @@ plot_time = plot(
 ########################################################
 # Heterodyne Receiver
 ########################################################
-s_in = s_rf + s_rf_img
 
 s_mixed = s_in .* s_lo
 s_mixed_spec = fftshift(1 / length(s_mixed) * fft(s_mixed))
@@ -89,7 +90,7 @@ plot_freq = plot(
         scatter(x=Fs/N*(-N/2:N/2-1), y=abs.(s_mixed_image_reject_lowpass_spec), name="Mixer output Lowpass (Image Reject)"),
     ],
     Layout(
-        title="Zeitsignal",
+        title="Heterodyne Receiver",
         xaxis_title="Frequency (Hz)",
         yaxis_title="Amplitude",
         yaxis=attr(range=[0, 1])
@@ -124,10 +125,65 @@ plot_hartley_freq = plot(
         scatter(x=Fs/N*(-N/2:N/2-1), y=abs.(s_hartley_out_spec), name="Hartley Out"),
     ],
     Layout(
-        title="Zeitsignal",
+        title="Hartley Receiver",
         xaxis_title="Frequency (Hz)",
         yaxis_title="Amplitude",
         yaxis=attr(range=[0, 1])
+    )
+)
+
+########################################################
+# Direct Conversion
+########################################################
+
+s_direct_out = s_in .* s_rf   # s_lo = s_rf
+s_direct_out_spec = fftshift(1 / length(s_direct_out) * fft(s_direct_out))
+
+# plot results
+plot_direct_freq = plot(
+    [
+        scatter(x=Fs/N*(-N/2:N/2-1), y=abs.(s_direct_out_spec), name="Direct Conversion out"), 
+    ],
+    Layout(
+        title="Direct Conversion",
+        xaxis_title="Frequency (Hz)",
+        yaxis_title="Amplitude",
+        yaxis=attr(range=[0, 1])
+    )
+)
+
+########################################################
+# IQ (analog, digital (QAM) is boring...)
+########################################################
+
+# Inphase path
+s_i_mixed = s_in .* s_lo    # s_lo = cosine
+s_i_mixed_spec = fftshift(1 / length(s_i_mixed) * fft(s_i_mixed))
+
+# Quadratur path
+s_q_mixed = s_in .* real(Hilbert(s_lo) .* exp(im * deg2rad(90)))    # s_lo = -sin
+s_q_mixed_spec = fftshift(1 / length(s_q_mixed) * fft(s_q_mixed))
+
+s_iq_out = s_i_mixed + s_q_mixed * im
+s_iq_out_spec = fftshift(1 / length(s_iq_out) * fft(s_iq_out))
+
+# plot results
+plot_iq_freq = plot(
+    [
+        #scatter(x=Fs/N*(-N/2:N/2-1), y=abs.(s_i_mixed_spec), name="IQ Inphase"),
+        #scatter(x=Fs/N*(-N/2:N/2-1), y=abs.(s_q_mixed_spec), name="IQ Quadratur"),
+        scatter3d(x=Fs/N*(-N/2:N/2-1), y=abs.(s_iq_out_spec), z=abs.(s_iq_out_spec), name="IQ out", mode="lines"), 
+    ],
+    Layout(
+        title="IQ Demodulator",
+        xaxis=attr(title="Frequency (Hz)"),
+        yaxis=attr(title="Real Part", range=[0, 1]),
+        zaxis=attr(title="Imaginary Part", range=[0, 1]),
+        scene=attr(
+            xaxis_title="Real Part",
+            yaxis_title="Imaginary Part",
+            zaxis_title="Frequency (Hz)"
+        )
     )
 )
 
@@ -136,8 +192,8 @@ plot_hartley_freq = plot(
 ########################################################
 
 fig = [
-    plot_time plot_freq;
-    plot_hartley_freq plot_hartley_freq
+    plot_time plot_freq plot_hartley_freq plot_direct_freq;
+    plot_iq_freq
 ]
 relayout!(fig)
 
